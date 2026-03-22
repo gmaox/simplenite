feb3385119c9b9e802382797e9268f320e1f6307
gmaox <92640318+gmaox@users.noreply.github.com>
2026-03-22 22:36:23 +0800

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul
 echo 正在使用 PyInstaller 打包 ...
-C:\Users\86150\AppData\Local\Programs\Python\Python38\python.exe -m PyInstaller --add-data "fav.ico;." --add-data "1.png;." --add-data "pssuspend64.exe;." -w DesktopGame.py -i "fav.ico" --uac-admin --noconfirm
+C:\Users\86150\AppData\Local\Programs\Python\Python38\python.exe -m PyInstaller --add-data "fav.ico;." --add-data "1.png;." --add-data "pssuspend64.exe;." -w Simplenite.py -i "fav.ico" --uac-admin --noconfirm
 if %ERRORLEVEL% equ 0 (
     echo.
     echo 打包完成。输出目录: dist\QuickStreamAppAdd.exe
